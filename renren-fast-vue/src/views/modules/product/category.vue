0fbb976e6b533a603b023cc58d2e15918e70e5d8
Jing Jin <johnnyjinjing@gmail.com>
2021-08-25 15:20:55 -0700

    added basic product update function

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -19,6 +19,9 @@
           >
             Append
           </el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            Edit
+          </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -30,22 +33,35 @@
         </span>
       </span>
     </el-tree>
-    <el-dialog title="" :visible.sync="dialogVisible" width="30%">
+    <el-dialog
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
       <el-form :model="category">
         <el-form-item label="New category name:">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="Icon:">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Unit:">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="addCategory">Confirm</el-button>
+        <el-button type="primary" @click="submitData">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
 export default {
   components: {},
   props: {},
@@ -58,9 +74,14 @@ export default {
         catLevel: 0,
         showStatus: 1,
         sort: 0,
+        catId: null,
+        productUnit: "",
+        icon: "",
       },
       expandedKey: [],
+      dialogTitle: "",
       dialogVisible: false,
+      dialogType: "",
       defaultProps: {
         children: "children",
         label: "name",
@@ -77,9 +98,17 @@ export default {
       });
     },
     append(data) {
+      this.dialogType = "add";
+      this.dialogTitle = "Add Category";
       this.dialogVisible = true;
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.catId = null;
+      this.category.name = "";
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.category.sort = 0;
+      this.category.showStatus = 1;
     },
     addCategory() {
       this.$http({
@@ -95,6 +124,48 @@ export default {
         this.getMenus();
         this.expandedKey = [this.category.parentCid];
       });
+    },
+    edit(data) {
+      this.dialogTitle = "Edit Category";
+      this.dialogType = "edit";
+      this.dialogVisible = true;
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+        this.category.catLevel = data.data.catLevel;
+        this.category.sort = data.data.sort;
+        this.category.showStatus = data.data.showStatus;
+      });
+    },
+    editCategory() {
+      var { catId, name, icon, productUnit } = this.category;
+
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: `Product is updated`,
+          type: "success",
+        });
+        this.dialogVisible = false;
+        this.getMenus();
+        this.expandedKey = [this.category.catId];
+      });
+    },
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      } else if (this.dialogType == "edit") {
+        this.editCategory();
+      }
     },
     remove(node, data) {
       this.$confirm(
